8aa53fa5be3b7553378f289574128d8718ce28e6
root <root@DESKTOP-AAQPJE5.localdomain>
2022-08-06 15:57:08 -0500

    implement add toy to finch functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
   path('toys/', views.ToyList.as_view(), name='toys_index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+  path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -18,9 +18,10 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', { 
-    'finch': finch, 'feeding_form': feeding_form
+    'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have
   })
 
 def add_feeding(request, finch_id):
@@ -31,9 +32,13 @@ def add_feeding(request, finch_id):
     new_feeding.save()
   return redirect('finches_detail', finch_id=finch_id)
 
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('finches_detail', finch_id=finch_id)
+
 class FinchCreate(CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
   success_url = '/finches/'
 
 class FinchUpdate(UpdateView):
